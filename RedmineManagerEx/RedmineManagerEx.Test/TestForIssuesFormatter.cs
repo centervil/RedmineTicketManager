584eb8b94683f790b440c87fb4bc03c008095865
ak-nkmr <centervil.en@gmail.com>
2019-12-09 01:32:46 +0900

    IssuesFormatterのコンストラクタ定義 #1

---
+++ RedmineManagerEx/RedmineManagerEx.Test/TestForIssuesFormatter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedmineManagerEx.Test
+{
+    [TestClass]
+    public class TestForIssuesFormatter
+    {
+        [TestMethod]
+        public void DataSourceToIssues()
+        {
+            var dataSource = new DataTable();
+            var formatter = new IssuesFormatter(dataSource);
+            var IssueList = formatter.IssueList;
+        }
+    }
+}
---
+++ RedmineManagerEx/RedmineManagerEx/IssuesFormatter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Redmine.Net.Api.Types;
+
+namespace RedmineManagerEx
+{
+    public class IssuesFormatter
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IssuesFormatter"/> class.
+        /// </summary>
+        /// <param name="dataSource">IssueListに変換する表形式データ</param>
+        public IssuesFormatter(DataTable dataSource)
+        {
+            this.DataSource = dataSource;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IssuesFormatter"/> class.
+        /// </summary>
+        /// <param name="issues">表形式データに変換するIssueList</param>
+        public IssuesFormatter(List<ICloneable> issues)
+        {
+            this.IssueList = issues;
+        }
+
+        public DataTable DataSource { get; private set; }
+
+        public List<ICloneable> IssueList { get; private set; }
+    }
+}
